2626dd38fe06596365651c8dbc2bb4ec87d6bfda
David Dewhirst <dsdewhir@gmail.com>
2013-11-01 21:29:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaCatrina::Application.config.secret_token = 'c67e9e5f78d5dca1537dc77449917bd78e3efb36c4d4148f905e9d4595039a402b71dae48be352626e51751f996d2e089aaf1df2a13d53ac73f47f53a675bdd7'
